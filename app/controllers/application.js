3b63271fa4b608afd0f7dfc69adbfa0fc8450538
Caner Uguz <caneruguz@gmail.com>
2017-01-04 15:13:07 -0500

    Add style and layout changes to index

---
+++ app/controllers/application.js
@@ -0,0 +1,5 @@
+import Ember from 'ember';
+import OSFAgnosticAuthControllerMixin from 'ember-osf/mixins/osf-agnostic-auth-controller';
+
+export default Ember.Controller.extend(OSFAgnosticAuthControllerMixin, {
+});
