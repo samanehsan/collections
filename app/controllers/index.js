a39e991a1355e8502c2f23a6dbdab312c61ba145
Caner Uguz <caneruguz@gmail.com>
2017-01-04 16:45:09 -0500

    Add workign filter for collectiosn

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,4 +2,18 @@ import Ember from 'ember';
 import OSFAgnosticAuthControllerMixin from 'ember-osf/mixins/osf-agnostic-auth-controller';
 
 export default Ember.Controller.extend(OSFAgnosticAuthControllerMixin, {
+    modelCache : null,
+    filterText : '',
+    actions : {
+        filter () {
+            let model = this.get('model');
+            let text = this.get('filterText').toLowerCase();
+            if(this.get('modelCache') === null){
+                this.set('modelCache', model);
+            }
+            this.set('model', this.get('modelCache').filter(function(item){
+                return item.title.includes(text);
+            }));
+        }
+    }
 });
