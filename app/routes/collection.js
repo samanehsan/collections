29833a31af4cdf7f4b318e0807378abbd373face
Caner Uguz <caneruguz@gmail.com>
2017-01-10 14:54:29 -0500

    Add mock type

--- app/routes/collection.js
+++ app/routes/collection.js
@@ -3,10 +3,12 @@ import faker from 'faker';
 
 export default Ember.Route.extend({
     generateFake () {
+        let types = ['project', 'preprint', 'registration', 'file', 'person'];
         let item = {
             title: faker.lorem.words(),
             description: faker.lorem.sentences(),
-            tags : faker.lorem.words().split(' ')
+            tags : faker.lorem.words().split(' '),
+            type : types[Math.floor(Math.random()*types.length)]
         };
         return item;
     },
