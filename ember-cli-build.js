288c0fa5d3fb9caac82f0b13b941e3c6b96869d1
Caner Uguz <caneruguz@gmail.com>
2017-01-10 10:15:37 -0500

    Add layers on index and remove bootstrao theme css

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -21,7 +21,6 @@ module.exports = function(defaults) {
   // along with the exports of each module as its value.
 
     app.import('bower_components/bootstrap/dist/css/bootstrap.css');
-    app.import('bower_components/bootstrap/dist/css/bootstrap-theme.css');
     app.import('bower_components/bootstrap/dist/js/bootstrap.js');
 
     app.import('bower_components/osf-style/css/base.css');
