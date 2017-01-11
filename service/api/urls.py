6141b6bb95f4c5f615a5a40e0485ce9159b3b449
Saman Ehsan <se8ea@virginia.edu>
2017-01-11 13:52:10 -0500

    Remove /items and /items/<item_id> routes
    Since items must belong to a collection, the
    top level routes for items are not needed.

--- service/api/urls.py
+++ service/api/urls.py
@@ -6,7 +6,5 @@ urlpatterns = [
     url(r'^collections/$', views.CollectionList.as_view(), name='collection-list'),
     url(r'^collections/(?P<pk>\w+)/$', views.CollectionDetail.as_view(), name='collection-detail'),
     url(r'^collections/(?P<pk>\w+)/items/$', views.CollectionItemList.as_view(), name='collection-items'),
-    url(r'^collections/(?P<pk>\w+)/items/(?P<item_id>\w+)/$', views.ItemDetail.as_view(), name='collection-item-detail'),
-    url(r'^items/$', views.ItemList.as_view(), name='item-list'),
-    url(r'^items/(?P<item_id>\w+)/$', views.ItemDetail.as_view(), name='item-detail'),
+    url(r'^collections/(?P<pk>\w+)/items/(?P<item_id>\w+)/$', views.ItemDetail.as_view(), name='collection-item-detail')
 ]
