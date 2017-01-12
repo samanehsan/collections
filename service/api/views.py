c03aa63318f0a5c85135691b410858d9e830d96f
Saman Ehsan <se8ea@virginia.edu>
2017-01-12 10:16:46 -0500

    Filter collections by title

--- service/api/views.py
+++ service/api/views.py
@@ -16,8 +16,14 @@ def api_root(request):
 
 class CollectionList(generics.ListCreateAPIView):
     """View list of collections and create a new collection. """
-    queryset = Collection.objects.all()
     serializer_class = CollectionSerializer
+
+    def get_queryset(self):
+        queryset = Collection.objects.all()
+        title = self.request.query_params.get('title', None)
+        if title is not None:
+            queryset = queryset.filter(title__icontains=title)
+        return queryset
 
 
 class CollectionDetail(generics.RetrieveUpdateDestroyAPIView):
