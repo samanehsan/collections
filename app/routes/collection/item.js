9edb35e87bf9911cb304432df5f703f3ae9fddb9
Caner Uguz <caneruguz@gmail.com>
2017-01-16 14:04:36 -0500

    Make mock pagination work

--- app/routes/collection/item.js
+++ app/routes/collection/item.js
@@ -2,19 +2,24 @@ import Ember from 'ember';
 import faker from 'faker';
 
 let types = ['project', 'preprint', 'registration', 'file', 'person'];
-
-let item = {
-    title: faker.lorem.words(),
-    description: faker.lorem.sentences(),
-    tags : faker.lorem.words().split(' '),
-    type : types[Math.floor(Math.random()*types.length)],
-    addedBy : faker.name.firstName() + faker.name.lastName(),
-    dateAdded : faker.date.past()
-
-};
+let status = ['pending', 'approved', 'other'];
+var item = Ember.Object.create({
+        title: faker.lorem.words()
+    });
 
 export default Ember.Route.extend({
-    model (){
+    model (params){
+        item.set('id', parseInt(params.item_id));
+        let id = item.get('id');
+        item.set('nextId', id > 0 ? id + 1 : null);
+        item.set('previousId', id > 1 ? id - 1 : null);
+        item.set('title', faker.lorem.words());
+        item.set('description', faker.lorem.sentences());
+        item.set('tags', faker.lorem.words().split(' '));
+        item.set('type', types[Math.floor(Math.random()*types.length)]);
+        item.set('addedBy', faker.name.firstName() + faker.name.lastName());
+        item.set('dateAdded', faker.date.past());
+        item.set('status', status[Math.floor(Math.random()*status.length)]);
         return item;
     },
     breadCrumb: {
