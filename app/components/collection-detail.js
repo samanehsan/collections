cac1c0a29ef439614701202d3d3cb470732730ea
Caner Uguz <caneruguz@gmail.com>
2017-01-17 11:32:07 -0500

    Add actions to collections detail save

--- app/components/collection-detail.js
+++ app/components/collection-detail.js
@@ -2,9 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     editMode : false,
+    modelCache : null,
     actions : {
-        toggleEdit () {
-            this.toggleProperty('editMode');
+        showEdit () {
+            this.set('modelCache', Ember.copy(this.get('model'), true));
+            console.log(this.get('modelCache'));
+            this.set('editMode', true);
+        },
+        cancelEdit() {
+            this.set('editMode', false);
+        },
+        saveEdit (){
+            this.set('model', Ember.copy(this.get('modelCache'), true));
+            this.set('modelCache', null);
+            this.set('editMode', false);
+
         }
     }
 });
