10f99c063cd5c7dd52093bed7b832e455b8d67bf
Saman Ehsan <se8ea@virginia.edu>
2017-01-17 13:53:43 -0500

    Add submission approval to admin view
    Allow admin users to view all submissions
    to their collections and to change the status
    of those submissions from the Django admin
    interface.

--- service/api/admin.py
+++ service/api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from models import Collection, Item
 
-# Register your models here.
+
+def approve_item(modeladmin, request, queryset):
+    queryset.update(status='approved')
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'type', 'created_by', 'status')
+    actions = [approve_item]
+
+    def get_search_results(self, request, queryset, search_term):
+        user = request.user
+        return self.model.objects.filter(collection__created_by=user), True
+
+admin.site.register(Item, ItemAdmin)
