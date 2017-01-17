7750e89b86ac9be89ca1633afaf15d9e0f1e42b5
Caner Uguz <caneruguz@gmail.com>
2017-01-17 14:08:06 -0500

    Add multiple selection for organizing

--- app/components/collection-item.js
+++ app/components/collection-item.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    selected: false,
+    actions : {
+        markSelected () {
+            this.toggleProperty('selected');
+        }
+    }
 });
