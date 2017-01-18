cf70b11813d87c9eba9f5728dfbb2602fc1a7a4c
Caner Uguz <caneruguz@gmail.com>
2017-01-18 09:16:51 -0500

    Redo add item layer

--- app/controllers/collection/index.js
+++ app/controllers/collection/index.js
@@ -1,17 +1,37 @@
 import Ember from 'ember';
+import faker from 'faker';
+
+let types = ['project', 'preprint', 'registration', 'file', 'person'];
 
 export default Ember.Controller.extend({
-    searchGuid : 'fkat6',
-    loadingGuid : false,
-    organizeMode : false,
-    actions : {
+    searchGuid: '',
+    loadingGuid: false,
+    organizeMode: false,
+    showAddItemDetails: false,
+    fakeNewNode: {},
+    actions: {
         findNode () {
-            this.set('loadingGuid', true);
-            console.log(this.store.findRecord('node', this.get('searchGuid')));
+            this.set('fakeNewNode',
+            {
+                title:  faker.lorem.words(),
+                description: faker.lorem.sentences(),
+                tags: faker.lorem.words().split(' '),
+                type: types[Math.floor(Math.random()*types.length)],
+                addedBy: faker.name.firstName() + faker.name.lastName()
+            });
+            this.toggleProperty('showAddItemDetails');
+
         },
         toggleOrganizeMode () {
             this.toggleProperty('organizeMode');
-            console.log('asad')
+        },
+        addToList(){
+            let list = this.get('model.list');
+            list.addObject(this.get('fakeNewNode'));
+            console.log(this.get('model'), list);
+            this.set('showAddItemDetails', false);
+            this.set('fakeNewNode', {});
+
         }
     }
 });
