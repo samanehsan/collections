c9e011dc26394ec54c8401be21e52cb45bd300bc
Caner Uguz <caneruguz@gmail.com>
2017-01-24 16:05:06 -0500

    Get data from server

--- client/app/routes/index.js
+++ client/app/routes/index.js
@@ -1,9 +1,11 @@
 import Ember from 'ember';
-import faker from 'faker';
 
 export default Ember.Route.extend({
     model () {
-        return this.get('store').findAll('collection');
+        return this.store.findAll('collection').then(function(data){
+            return data;
+        });
+
         // return $.getJSON('http://127.0.0.1:8000/api/collections').then(result => {
         //      return result.data;
         //    });
