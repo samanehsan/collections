76347c74279882fb095e552b581c688c4011546c
Caner Uguz <caneruguz@gmail.com>
2017-02-08 15:48:23 -0500

    Save updates to collection detail

--- client/app/routes/collection.js
+++ client/app/routes/collection.js
@@ -1,11 +1,6 @@
 import Ember from 'ember';
 
 let formatData = function (collection){
-    // Split tags string into array
-    let tags = collection.get('tags').split(',');
-    let newTags = tags.splice(0,tags.length-1);
-    collection.set('formattedTags', newTags);
-
     //Format dates
     let dateCreated = new Date(collection.get('dateCreated'));
     collection.set('dateCreatedReadable', dateCreated.toDateString());
