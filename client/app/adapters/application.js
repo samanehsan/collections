f553e569ddb7718a9491b3025b8d2e76004626ad
Caner Uguz <caneruguz@gmail.com>
2017-02-08 15:48:23 -0500

    Save updates to collection detail

--- client/app/adapters/application.js
+++ client/app/adapters/application.js
@@ -2,5 +2,8 @@ import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
     host: 'http://127.0.0.1:8000',
-    namespace: 'api'
+    namespace: 'api',
+    headers :{
+        'Authorization' : 'Basic YWRtaW46YWRtaW4yMjkwMw=='
+    }
 });
