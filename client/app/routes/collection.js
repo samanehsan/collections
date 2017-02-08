507098149f674ceb21e4ddf9056338a25b05555c
Caner Uguz <caneruguz@gmail.com>
2017-02-08 11:05:48 -0500

    Add formatted dates [breaks edit mode ]

--- client/app/routes/collection.js
+++ client/app/routes/collection.js
@@ -3,9 +3,15 @@ import faker from 'faker';
 
 
 let formatData = function (collection){
+    // Split tags into array
     let tags = collection.get('tags').split(',');
     let newTags = tags.splice(0,tags.length-1);
     collection.set('formattedTags', newTags);
+    //Format dates
+    let dateCreated = new Date(collection.get('dateCreated'));
+    collection.set('dateCreatedReadable', dateCreated.toDateString());
+    let dateUpdated = new Date(collection.get('dateUpdated'));
+    collection.set('dateUpdatedReadable', dateUpdated.toDateString());
     return collection;
 };
 
--- client/app/serializers/application.js
+++ client/app/serializers/application.js
@@ -1,4 +1,8 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.JSONAPISerializer.extend({
+    keyForAttribute(key) {
+         return Ember.String.underscore(key);
+     }
 });
