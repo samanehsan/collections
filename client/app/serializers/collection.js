3547f2ff0cb9f5ebb53ec1181ace0196c7fd4773
Saman Ehsan <se8ea@virginia.edu>
2017-02-09 17:06:11 -0500

    Add groups and items to ember collection model
    Also modify the JSON response from the collections
    endpoint in the ember collection serializer to only
    have related resource links for items and groups
    (not embedded records) so that ember requests the
    resources from those links.

--- client/app/serializers/collection.js
+++ client/app/serializers/collection.js
@@ -1,4 +1,18 @@
 import ApplicationSerializer from './application';
 
 export default ApplicationSerializer.extend({
+  normalizeFindRecordResponse(store, type, payload) {
+    // TODO: Fix format in API instead of here
+    var collectionID = payload['data']['id'];
+    payload['data']['relationships']['groups'] = {
+        meta: payload['data']['relationships']['groups']['meta'],
+        links: payload['data']['relationships']['groups']['links']
+    };
+    payload['data']['relationships']['items'] = {
+        links: payload['data']['relationships']['items']['links'] = {
+           'related': '/api/collections/' + collectionID + '/items/'
+        }
+    };
+    return this._super(...arguments);
+  }
 });
