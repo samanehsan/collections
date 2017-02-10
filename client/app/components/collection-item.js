078533b3e97c6956993f8838b24b8af55416c75e
Saman Ehsan <se8ea@virginia.edu>
2017-02-10 11:24:11 -0500

    List collection groups

--- client/app/components/collection-item.js
+++ client/app/components/collection-item.js
@@ -1,6 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    item: null,
+    isGroup: Ember.computed(function() {
+       var type = this.get('item').get('type');
+       return !type;
+    }),
+    type: Ember.computed(function() {
+        var item = this.get('item');
+        var type = item.get('type');
+        return type ? type : 'group'
+    }),
     selected: false,
     actions : {
         markSelected (item) {
