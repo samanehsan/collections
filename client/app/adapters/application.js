d02960847bd1bafaf475643034444b340974bc03
Caner Uguz <caneruguz@gmail.com>
2017-02-13 16:41:06 -0500

    Make authorization environment variable

--- client/app/adapters/application.js
+++ client/app/adapters/application.js
@@ -1,9 +1,10 @@
 import DS from 'ember-data';
+import config from '../config/environment';
 
 export default DS.JSONAPIAdapter.extend({
     host: 'http://127.0.0.1:8000',
     namespace: 'api',
     headers :{
-        'Authorization' : 'Basic ASDASDASDASDASD'
+        'Authorization' : config.headerAuth
     }
 });
