eb24477563046f8391ebc47ad9045b42da2bdf74
Saman Ehsan <se8ea@virginia.edu>
2017-02-14 14:42:43 -0500

    Fix actions on group detail page

--- client/app/controllers/collection/group/index.js
+++ client/app/controllers/collection/group/index.js
@@ -1,11 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    organizeMode: false,
     showDeleteConfirmation: false,
     actions : {
-        toggleDeleteConfirmation(){
-            this.toggleProperty('showDeleteConfirmation');
-        },
         deletePartial(){
             // for the moment this will do the same as deletefull.
         },
