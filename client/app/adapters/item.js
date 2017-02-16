c11690938f66b8cdf65400abe64f60f730f32be9
Caner Uguz <caneruguz@gmail.com>
2017-02-16 10:15:05 -0500

    Add proper variable names to buildUrl

--- client/app/adapters/item.js
+++ client/app/adapters/item.js
@@ -1,11 +1,11 @@
 import Application from './application';
 
 export default Application.extend({
-    buildURL(_, __, ___, requestType) {
+    buildURL(modelName, id, snapshot, requestType) {
         var defaultUrl = this._super(...arguments);
         // Embed linked_nodes
         if(requestType === 'createRecord'){
-            return this.get('host') + '/api/collections/' + ___.record.get('collection').get('id') + '/items/';
+            return this.get('host') + '/api/collections/' + snapshot.record.get('collection').get('id') + '/items/';
         }
         return defaultUrl + '/';
     }
