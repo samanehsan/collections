5e53e3cbf800d715a6215862cc9144e95e2df425
Caner Uguz <caneruguz@gmail.com>
2017-02-16 10:04:17 -0500

    Get individual item view from ember data

--- client/app/adapters/item.js
+++ client/app/adapters/item.js
@@ -2,7 +2,11 @@ import Application from './application';
 
 export default Application.extend({
     buildURL(_, __, ___, requestType) {
+        var defaultUrl = this._super(...arguments);
         // Embed linked_nodes
-        return this.get('host') + '/api/collections/' + ___.record.get('collection').get('id') + '/items/';
+        if(requestType === 'createRecord'){
+            return this.get('host') + '/api/collections/' + ___.record.get('collection').get('id') + '/items/';
+        }
+        return defaultUrl + '/';
     }
 });
