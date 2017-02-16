e5cbf5f6d91d7c92ec579464b828854724db32f1
Caner Uguz <caneruguz@gmail.com>
2017-02-16 16:26:04 -0500

    Fix view with wiki content

--- client/app/components/item-detail-page.js
+++ client/app/components/item-detail-page.js
@@ -2,7 +2,28 @@ import Ember from 'ember';
 import loadAll from 'ember-osf/utils/load-relationship';
 
 export default Ember.Component.extend({
-    getAuthors: Ember.observer('item.node', function() {
+    wikiContent: null,
+    formatWikiData: Ember.observer('item.node.wikis', function(){
+        let node = this.get('item.node');
+        let wikis = node.get('wikis');
+        let item = this.get('item');
+        wikis.then(function(result){
+            this.set('wikiContent', 'Wiki content will come here');
+            // if(item.hasWiki && result.objectAt(0)){
+            //     let url = result.objectAt(0).get('links.download');
+            //     Ember.$.ajax({
+            //         method: 'GET',
+            //         url: url,
+            //         xhrFields: {
+            //             withCredentials: true
+            //         }
+            //     }).done(data => {
+            //         this.set('wikiContent', data);
+            //     });
+            // }
+        }.bind(this));
+    }),
+    formatNodeData: Ember.observer('item.node', function() {
         // Cannot be called until node has loaded!
         const node = this.get('item.node');
         if (!node) { return [];}
@@ -11,5 +32,8 @@ export default Ember.Component.extend({
         loadAll(node, 'contributors', contributors).then(() =>
             this.set('authors', contributors)
         );
+
+
+
     })
 });
--- client/app/routes/collection/item.js
+++ client/app/routes/collection/item.js
@@ -1,12 +1,13 @@
 import Ember from 'ember';
 
-const hasFileList = ['preprint', 'file'];
-
+const hasFileList = ['preprint', 'file', 'poster'];
+const hasWikiList = ['project', 'registration'];
 export default Ember.Route.extend({
     model (params){
       let self = this;
       return this.store.findRecord('item', params.item_id).then(function(item){
-        item.set('hasFile', hasFileList.includes(item.get('category')));
+        item.set('hasFile', hasFileList.includes(item.get('type')));
+        item.set('hasWiki', hasWikiList.includes(item.get('type')));
         self.get('store').findRecord('node', item.get('source_id')).then(function(node){
           item.set('node', node);
         });
