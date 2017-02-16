4de2b13c6313c2cb2b6f377eb5a699d046592ef6
Caner Uguz <caneruguz@gmail.com>
2017-02-16 11:44:16 -0500

    Get authors of the item [code from preprints]

--- client/app/components/item-detail-page.js
+++ client/app/components/item-detail-page.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
+import loadAll from 'ember-osf/utils/load-relationship';
+
 
 export default Ember.Component.extend({
+    getAuthors: Ember.observer('item.node', function() {
+        // Cannot be called until node has loaded!
+        const node = this.get('item.node');
+        if (!node) return [];
+
+        const contributors = Ember.A();
+        loadAll(node, 'contributors', contributors).then(() =>
+            this.set('authors', contributors)
+        );
+    })
 });
