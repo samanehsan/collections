a637501ac7980ff7992d1dbfff8f5224d56b16ce
Caner Uguz <caneruguz@gmail.com>
2017-02-20 11:28:18 -0500

    Pass correct parameter to group item

--- client/app/routes/collection/group/item.js
+++ client/app/routes/collection/group/item.js
@@ -5,7 +5,7 @@ const hasWikiList = ['project', 'registration'];
 export default Ember.Route.extend({
     model (params){
       let self = this;
-      return this.store.findRecord('item', params.item_id).then(function(item){
+      return this.store.findRecord('item', params.group_item_id).then(function(item){
         item.set('hasFile', hasFileList.includes(item.get('type')));
         item.set('hasWiki', hasWikiList.includes(item.get('type')));
         self.get('store').findRecord('node', item.get('source_id')).then(function(node){
