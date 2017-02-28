5be13cbe61fc4ed80dd6f7c0c93016b332e7aaed
Caner Uguz <caneruguz@gmail.com>
2017-02-28 15:24:46 -0500

    Add website functionality

--- client/app/controllers/collection/add.js
+++ client/app/controllers/collection/add.js
@@ -3,10 +3,35 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     addMethod: 'select', // 'select' or 'create'
     methodSelected: false,
+    urlTitle: '',
+    urlAddress: '',
+    urlSaveErrors: null,
+    clearInputs () {
+        this.set('urlTitle', '');
+        this.set('urlAddress', '');
+    },
     actions:{
         updateProperty(oldValue, newValue){
             this.set(oldValue, newValue);
             this.set('methodSelected', true); // Change view to show the methods
+        },
+        addWebsite (){
+            let self = this;
+            let item = this.get('store').createRecord('item', {
+                title: this.get('urlTitle'),
+                type: 'website',
+                metadata: '',
+                status: 'pending',
+                url: this.get('urlAddress'),
+                source_id: this.get('urlAddress'),
+                collection : this.get('model')
+            });
+            item.save().then(function(){
+                self.transitionToRoute('collection', self.get('model.id'));
+            }).catch(function(error){
+                self.set('urlSaveErrors', error.errors);
+            });
+            this.clearInputs();
         }
     }
 });
