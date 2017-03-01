eb46340128599b43e68c052fea6cb171ecd736ac
Joshua Bird <joshua.thomas.bird@gmail.com>
2017-03-01 11:29:18 -0500

    Update application.js

--- client/app/routes/application.js
+++ client/app/routes/application.js
@@ -4,12 +4,4 @@ import OsfTokenLoginRouteMixin from 'ember-osf/mixins/osf-token-login-route';
 
 export default Ember.Route.extend(OsfTokenLoginRouteMixin, {
 
-    model() {
-        //if (this.get('session.isAuthenticated')) {
-        //    return this.store.findRecord('user', 'me').then(function(data) {
-        //        return data;
-        //    });
-        //}
-    }
-
 });
