be2ed725e4f59867f9edd3c1af9618aa799e3755
Caner Uguz <caneruguz@gmail.com>
2017-03-03 12:11:40 -0500

    Remove website specific code from add.js
    … because website is its own component now

--- client/app/controllers/collection/add.js
+++ client/app/controllers/collection/add.js
@@ -4,39 +4,13 @@ export default Ember.Controller.extend({
     addMethod: 'select', // 'select' or 'create'
     methodSelected: false,
     type: Ember.computed('model.settings', function(){
-        let type = this.get('model.settings.collectionType').toLowerCase;
+        let type = this.get('model.settings.collectionType').toLowerCase();
         return  type === 'preprint' || type === 'project' ? 'node' : type;
     }),
-    urlTitle: '',
-    urlAddress: '',
-    urlDescription: '',
-    urlSaveErrors: null,
-    clearInputs () {
-        this.set('urlTitle', '');
-        this.set('urlAddress', '');
-    },
     actions:{
         updateProperty(oldValue, newValue){
             this.set(oldValue, newValue);
             this.set('methodSelected', true); // Change view to show the methods
-        },
-        addWebsite (){
-            let self = this;
-            let item = this.get('store').createRecord('item', {
-                title: this.get('urlTitle'),
-                type: 'website',
-                metadata: this.get('urlDescription'),
-                status: 'pending',
-                url: this.get('urlAddress'),
-                source_id: this.get('urlAddress'),
-                collection : this.get('model')
-            });
-            item.save().then(function(){
-                self.transitionToRoute('collection', self.get('model.id'));
-            }).catch(function(error){
-                self.set('urlSaveErrors', error.errors);
-            });
-            this.clearInputs();
         }
     }
 });
