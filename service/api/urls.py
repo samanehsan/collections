7f1b4133e10f6938e31a7cf76c5bdc4b86fddf92
Saman Ehsan <se8ea@virginia.edu>
2017-03-03 15:00:16 -0500

    Remove unused collection user view

--- service/api/urls.py
+++ service/api/urls.py
@@ -17,5 +17,4 @@ urlpatterns = [
 
     url(r'^items/$', api.views.ItemList.as_view(), name='item-list'),
     url(r'^items/(?P<item_id>\w+)/$', api.views.ItemDetail.as_view(), name='item-detail'),
-    url(r'^collections/(?P<pk>\w+)/owner/$', api.views.CollectionUserDetail.as_view(), name='collection-user'),
 ]
