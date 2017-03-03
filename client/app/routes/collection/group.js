e5fa96d5157560f5cf63d8ef521b3dcd7c55edfd
Saman Ehsan <se8ea@virginia.edu>
2017-03-03 11:07:28 -0500

    Fix group date formatting

--- client/app/routes/collection/group.js
+++ client/app/routes/collection/group.js
@@ -1,10 +1,9 @@
 import Ember from 'ember';
-import {formatDate} from 'collections/utils/formatDate';
 
 export default Ember.Route.extend({
     model(params){
         return this.get('store').findRecord('group', params.group_id).then(group => {
-            return formatDate(group);
+            return group;
         });
     }
 });
