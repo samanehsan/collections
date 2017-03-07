06750ee941305e15b055d9ed0ce9bc83d89ca87b
Saman Ehsan <se8ea@virginia.edu>
2017-03-07 17:40:47 -0500

    Add coll admins via django admin
    Allow superusers in django admin to
    easily give multiple users permission
    to approve/reject items in a given
    collection.

--- service/api/admin.py
+++ service/api/admin.py
@@ -1,10 +1,25 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.admin.helpers import ActionForm
 from api.models import Collection, Item
-from guardian.shortcuts import get_objects_for_user
+from guardian.shortcuts import get_objects_for_user, assign_perm
 
 
 def approve_item(modeladmin, request, queryset):
     queryset.update(status='approved')
+
+
+def add_admins(modeladmin, request, queryset):
+    collection_id = request.POST.get('collection_id')
+    collection = Collection.objects.get(id=collection_id)
+    for user in queryset:
+        assign_perm('api.approve_items', user, collection)
+
+
+class AdminForm(ActionForm):
+    collection_id = forms.CharField()
 
 
 class ItemAdmin(admin.ModelAdmin):
@@ -16,4 +31,12 @@ class ItemAdmin(admin.ModelAdmin):
         collections = get_objects_for_user(user, 'api.approve_items')
         return self.model.objects.filter(collection__in=collections), True
 
+
+class OSFUserAdmin(UserAdmin):
+    model = get_user_model()
+    action_form = AdminForm
+    actions = [add_admins]
+
+
 admin.site.register(Item, ItemAdmin)
+admin.site.register(get_user_model(), OSFUserAdmin)
