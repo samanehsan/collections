5577982c6b99d3e02bd81ec685c4db5bd02ee01a
Caner Uguz <caneruguz@gmail.com>
2017-03-29 15:43:14 -0400

    Implement basic branded landing pages

--- client/app/components/landing-list/component.js
+++ client/app/components/landing-list/component.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    buttonStyle: Ember.computed('branding.colors', function() {
+       //var bgcolor = escapeCSS(this.get('branding.colors.primary'));
+       return Ember.String.htmlSafe("background-color: " + this.get('branding.colors.primary'));
+     })
 });
--- client/app/utils/sampleLandingSettings.js
+++ client/app/utils/sampleLandingSettings.js
@@ -1,35 +1,84 @@
 let collectionSettings = {
     branding : {
         colors: {
-            primary: '#ffffff',
-            secondary: '#eeeeee'
+            primary: '#215264',
+            secondary: '#eeeeee',
+            background: '#e14b5a',
+            backgroundText: '#ffffff'
         },
         logo: {
-            text: 'Some Service',
-            url: '/images/logo.png'
+            label: 'socarxiv',
+            url: 'https://osf.io/preprints/assets/img/provider_logos/socarxiv-small-8bb4e49d8c3bb92a90798039fd604ca680235083.png',
+            height: '140px'
         }
     },
     landingComponents: [
         {
-            type: 'landing-hero'
+            type: 'landing-hero',
+            title: 'You can override collection title here',
+            tagline: 'Open archive of the social sciences',
+            useLogo: true
         },
         {
             type: 'landing-list', // Corresponds to a component name in the app. Component expects data to fit it's template
             title: 'Subjects',
-            data: ['Biology', 'Geology']
+            data: [
+                    {
+                        name: 'Arts and Humanities',
+                        label: 'Fine Arts, History, Music, Philosophy, Religion',
+                        link: '/preprints/socarxiv/discover?subject=Arts%20and%20Humanities'
+                    },
+                    {
+                        name: 'Arts and Humanities',
+                        label: 'Fine Arts, History, Music, Philosophy, Religion',
+                        link: '/preprints/socarxiv/discover?subject=Arts%20and%20Humanities'
+                    },
+                    {
+                        name: 'Arts and Humanities',
+                        label: 'Fine Arts, History, Music, Philosophy, Religion',
+                        link: '/preprints/socarxiv/discover?subject=Arts%20and%20Humanities'
+                    },
+                    {
+                        name: 'Arts and Humanities',
+                        label: 'Fine Arts, History, Music, Philosophy, Religion',
+                        link: '/preprints/socarxiv/discover?subject=Arts%20and%20Humanities'
+                    },
+                 ]
         },
         {
             type:  'landing-board',
             title: 'Board of this Service',
             data: [
-                {firstName: 'Donna', lastName: 'Brown'},
-                {firstName: 'Maria', lastName: 'Gonzales'},
-                {firstName: 'George', lastName: 'Wu'}
+                {
+                  firstName: 'Donna',
+                  lastName: 'Brown',
+                  institution: 'Massachusetts Institute of Technology'
+                }, {
+                  firstName: 'Maria',
+                  lastName: 'Gonzales',
+                  institution: 'University of North Carolina at Chapel Hill'
+                }, {
+                  firstName: 'George',
+                  lastName: 'Wu',
+                  institution: 'Massachusetts Institute of Technology'
+                }, {
+                  firstName: 'Donna',
+                  lastName: 'Brown',
+                  institution: 'University of Maryland, College Park'
+                }, {
+                  firstName: 'Maria',
+                  lastName: 'Gonzales',
+                  institution: 'University of North Carolina at Chapel Hill'
+                }, {
+                  firstName: 'George',
+                  lastName: 'Wu',
+                  institution: 'University of Maryland, College Park'
+                }
             ]
         },
         {
             type: 'landing-copyright',
-            links: [
+            data: [
                 {
                     label: 'Support',
                     url: '/support'
