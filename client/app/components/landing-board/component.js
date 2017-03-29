f8b6f7e72dd6b359e4a4d1e6de754dc1ed214a96
Caner Uguz <caneruguz@gmail.com>
2017-03-29 15:56:38 -0400

    Split board list into two columns

--- client/app/components/landing-board/component.js
+++ client/app/components/landing-board/component.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+    listColumns: Ember.computed('componentSettings.data', function() {
+        let column = {};
+        let list = this.get('componentSettings.data');
+        let splitIndex = Math.round(list.length/2);
+        column.left = list.splice(0,splitIndex);
+        column.right = list;
+       return column;
+     })
 });
--- client/app/utils/sampleLandingSettings.js
+++ client/app/utils/sampleLandingSettings.js
@@ -1,101 +1,126 @@
 let collectionSettings = {
-    branding : {
-        colors: {
-            primary: '#215264',
-            secondary: '#eeeeee',
-            background: '#e14b5a',
-            backgroundText: '#ffffff'
-        },
-        logo: {
-            label: 'socarxiv',
-            url: 'https://osf.io/preprints/assets/img/provider_logos/socarxiv-small-8bb4e49d8c3bb92a90798039fd604ca680235083.png',
-            height: '140px'
-        }
+  branding: {
+    colors: {
+      primary: '#215264',
+      secondary: '#eeeeee',
+      background: '#e14b5a',
+      backgroundText: '#ffffff'
     },
-    landingComponents: [
-        {
-            type: 'landing-hero',
-            title: 'You can override collection title here',
-            tagline: 'Open archive of the social sciences',
-            useLogo: true
+    logo: {
+      label: 'socarxiv',
+      url: 'https://osf.io/preprints/assets/img/provider_logos/socarxiv-small-8bb4e49d8c3bb92a90798039fd604ca680235083.png',
+      height: '140px'
+    }
+  },
+  landingComponents: [{
+      type: 'landing-hero',
+      title: 'You can override collection title here',
+      tagline: 'Open archive of the social sciences',
+      useLogo: true
+    },
+    {
+      type: 'landing-list', // Corresponds to a component name in the app. Component expects data to fit it's template
+      title: 'Subjects',
+      data: [{
+          name: 'Arts and Humanities',
+          label: 'Fine Arts, History, Music, Philosophy, Religion',
+          link: '/preprints/socarxiv/discover?subject=Arts%20and%20Humanities'
         },
         {
-            type: 'landing-list', // Corresponds to a component name in the app. Component expects data to fit it's template
-            title: 'Subjects',
-            data: [
-                    {
-                        name: 'Arts and Humanities',
-                        label: 'Fine Arts, History, Music, Philosophy, Religion',
-                        link: '/preprints/socarxiv/discover?subject=Arts%20and%20Humanities'
-                    },
-                    {
-                        name: 'Arts and Humanities',
-                        label: 'Fine Arts, History, Music, Philosophy, Religion',
-                        link: '/preprints/socarxiv/discover?subject=Arts%20and%20Humanities'
-                    },
-                    {
-                        name: 'Arts and Humanities',
-                        label: 'Fine Arts, History, Music, Philosophy, Religion',
-                        link: '/preprints/socarxiv/discover?subject=Arts%20and%20Humanities'
-                    },
-                    {
-                        name: 'Arts and Humanities',
-                        label: 'Fine Arts, History, Music, Philosophy, Religion',
-                        link: '/preprints/socarxiv/discover?subject=Arts%20and%20Humanities'
-                    },
-                 ]
+          name: 'Arts and Humanities',
+          label: 'Fine Arts, History, Music, Philosophy, Religion',
+          link: '/preprints/socarxiv/discover?subject=Arts%20and%20Humanities'
         },
         {
-            type:  'landing-board',
-            title: 'Board of this Service',
-            data: [
-                {
-                  firstName: 'Donna',
-                  lastName: 'Brown',
-                  institution: 'Massachusetts Institute of Technology'
-                }, {
-                  firstName: 'Maria',
-                  lastName: 'Gonzales',
-                  institution: 'University of North Carolina at Chapel Hill'
-                }, {
-                  firstName: 'George',
-                  lastName: 'Wu',
-                  institution: 'Massachusetts Institute of Technology'
-                }, {
-                  firstName: 'Donna',
-                  lastName: 'Brown',
-                  institution: 'University of Maryland, College Park'
-                }, {
-                  firstName: 'Maria',
-                  lastName: 'Gonzales',
-                  institution: 'University of North Carolina at Chapel Hill'
-                }, {
-                  firstName: 'George',
-                  lastName: 'Wu',
-                  institution: 'University of Maryland, College Park'
-                }
-            ]
+          name: 'Arts and Humanities',
+          label: 'Fine Arts, History, Music, Philosophy, Religion',
+          link: '/preprints/socarxiv/discover?subject=Arts%20and%20Humanities'
         },
         {
-            type: 'landing-copyright',
-            data: [
-                {
-                    label: 'Support',
-                    url: '/support'
-                },
-                {
-                    label: 'Contact',
-                    url: '/contact'
-                },
-                {
-                    label: 'Twitter',
-                    url: '/twitter/somearchive',
-                    icon: 'fa-twitter'
-                }
-            ]
+          name: 'Arts and Humanities',
+          label: 'Fine Arts, History, Music, Philosophy, Religion',
+          link: '/preprints/socarxiv/discover?subject=Arts%20and%20Humanities'
+        },
+      ]
+    },
+    {
+      type: 'landing-board',
+      title: 'Steering Committee',
+      data: [{
+        firstName: 'Donna',
+        lastName: 'Brown',
+        institution: 'Massachusetts Institute of Technology'
+      }, {
+        firstName: 'Maria',
+        lastName: 'Gonzales',
+        institution: 'University of North Carolina at Chapel Hill'
+      }, {
+        firstName: 'George',
+        lastName: 'Wu',
+        institution: 'Massachusetts Institute of Technology'
+      }, {
+        firstName: 'Donna',
+        lastName: 'Brown',
+        institution: 'University of Maryland, College Park'
+      }, {
+        firstName: 'Maria',
+        lastName: 'Gonzales',
+        institution: 'University of North Carolina at Chapel Hill'
+      }, {
+        firstName: 'George',
+        lastName: 'Wu',
+        institution: 'University of Maryland, College Park'
+      }, {
+        firstName: 'Maria',
+        lastName: 'Gonzales',
+        institution: 'University of North Carolina at Chapel Hill'
+      }, {
+        firstName: 'Maria',
+        lastName: 'Gonzales',
+        institution: 'University of North Carolina at Chapel Hill'
+      }, {
+        firstName: 'George',
+        lastName: 'Wu',
+        institution: 'University of Maryland, College Park'
+      }, {
+        firstName: 'Maria',
+        lastName: 'Gonzales',
+        institution: 'University of North Carolina at Chapel Hill'
+      }, {
+        firstName: 'Maria',
+        lastName: 'Gonzales',
+        institution: 'University of North Carolina at Chapel Hill'
+      }, {
+        firstName: 'George',
+        lastName: 'Wu',
+        institution: 'University of Maryland, College Park'
+      }, {
+        firstName: 'Maria',
+        lastName: 'Gonzales',
+        institution: 'University of North Carolina at Chapel Hill'
+      }, ]
+    },
+    {
+      type: 'landing-copyright',
+      data: [{
+          label: 'Support',
+          url: '/support'
+        },
+        {
+          label: 'Contact',
+          url: '/contact'
+        },
+        {
+          label: 'Twitter',
+          url: '/twitter/somearchive',
+          icon: 'fa-twitter'
         }
+      ]
+    }
 
-    ]
+  ]
 };
 
-export {collectionSettings};
+export {
+  collectionSettings
+};
