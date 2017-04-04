c53deb47b73304b52b581c297f188939f2d1e60d
Caner Uguz <caneruguz@gmail.com>
2017-04-04 10:34:27 -0400

    Add non-dismissable reminder that login is required.

--- client/app/routes/application.js
+++ client/app/routes/application.js
@@ -3,5 +3,15 @@ import Ember from 'ember';
 import OsfTokenLoginRouteMixin from 'ember-osf/mixins/osf-token-login-route';
 
 export default Ember.Route.extend(OsfTokenLoginRouteMixin, {
-
+    currentUser: Ember.inject.service('currentUser'),
+    model(){
+        let model = {};
+        model.loggedIn = false;
+        this.get('currentUser').load().then(function(result){
+            if(result.get('id')){
+                model.loggedIn = true;
+            }
+        });
+        return model;
+    }
 });
