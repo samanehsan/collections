233e0bbcb1f9645057a06ed61e8c5630882c5261
Caner Uguz <caneruguz@gmail.com>
2017-04-04 10:41:19 -0400

    Do better way of checking user log in

--- client/app/routes/application.js
+++ client/app/routes/application.js
@@ -5,13 +5,8 @@ import OsfTokenLoginRouteMixin from 'ember-osf/mixins/osf-token-login-route';
 export default Ember.Route.extend(OsfTokenLoginRouteMixin, {
     currentUser: Ember.inject.service('currentUser'),
     model(){
-        let model = {};
-        model.loggedIn = false;
-        this.get('currentUser').load().then(function(result){
-            if(result.get('id')){
-                model.loggedIn = true;
-            }
+        return this.get('currentUser').load().then(function(result){
+            return result.get('id') ? true : false;
         });
-        return model;
     }
 });
