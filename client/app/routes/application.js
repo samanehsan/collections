a5c6d340558b7cade8eb78bfe3fe5bbfb8944263
Caner Uguz <caneruguz@gmail.com>
2017-04-04 11:54:52 -0400

    Undo sign in alert
    it’s causing problems and probably not the best way to do things.

--- client/app/routes/application.js
+++ client/app/routes/application.js
@@ -3,10 +3,5 @@ import Ember from 'ember';
 import OsfTokenLoginRouteMixin from 'ember-osf/mixins/osf-token-login-route';
 
 export default Ember.Route.extend(OsfTokenLoginRouteMixin, {
-    currentUser: Ember.inject.service('currentUser'),
-    model(){
-        return this.get('currentUser').load().then(function(result){
-            return result.get('id') ? true : false;
-        });
-    }
+
 });
