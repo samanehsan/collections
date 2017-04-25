f75c2404b11719037c5f84b9b86ad6a5fe34e1dc
Saman Ehsan <se8ea@virginia.edu>
2017-04-25 17:08:00 -0400

    Create separate adapters for OSF models
    - Instead of making the application adapter host
      the collections API, make it the host for just the
      adapters for collection models (collection, group and
      item).
    - Add node, preprint and registration adapters with
      staging-api.osf.io as the host, that sends the bearer
      token with each request. Note that ajax withCredentials
      must be false!

--- client/app/adapters/collection.js
+++ client/app/adapters/collection.js
@@ -1,8 +1,18 @@
-import Application from './application';
+import Ember from 'ember';
+import DS from 'ember-data';
 
-export default Application.extend({
+
+export default DS.JSONAPIAdapter.extend({
+    session: Ember.inject.service(),
+    host: 'http://localhost:8000',
+    namespace: 'api',
+    ajax(url, method, hash) {
+        hash = hash || {};
+        hash.crossOrigin = true;
+        hash.xhrFields = { withCredentials: true };
+        return this._super(url, method, hash);
+    },
     buildURL() {
-        // Embed linked_nodes
         var base = this._super(...arguments);
         return `${base}/`;
     }
---
+++ client/app/adapters/node.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+import DS from 'ember-data';
+
+
+function getToken() {
+    var token;
+    var session = window.localStorage['ember_simple_auth:session'];
+    if (session) {
+        token = JSON.parse(session)['authenticated'];
+        if ('attributes' in token) {
+            return token['attributes']['accessToken'];
+        }
+        return token;
+    }
+}
+
+export default DS.JSONAPIAdapter.extend({
+    session: Ember.inject.service(),
+    host: 'https://staging-api.osf.io',
+    namespace: 'v2',
+    ajax(url, method, hash) {
+        hash = hash || {};
+        hash.crossOrigin = true;
+        hash.xhrFields = { withCredentials: false };
+        hash.headers = hash.headers || {};
+        hash.headers['AUTHORIZATION'] = 'Bearer ' + getToken();
+        return this._super(url, method, hash);
+    },
+    buildURL() {
+        var base = this._super(...arguments);
+        return `${base}/`;
+    }
+});
+
