60e4fd6c0d271cb1a4b977d989fb6b36da0635e8
Saman Ehsan <se8ea@virginia.edu>
2017-04-27 15:33:55 -0400

    Set ACCOUNT_LOGOUT_REDIRECT_URL
    The default redirect url is the root
    collections api url, which gives a 404
    error. Instead, set it to `/api/`. This
    does not affect redirects on the front-end.

--- service/service/settings/base.py
+++ service/service/settings/base.py
@@ -83,6 +83,7 @@ OAUTH2_PROVIDER = {
 }
 
 LOGIN_REDIRECT_URL = os.environ.get('LOGIN_REDIRECT_URL', 'http://localhost:4200/')
+ACCOUNT_LOGOUT_REDIRECT_URL = os.environ.get('ACCOUNT_LOGOUT_REDIRECT_URL', '/api/')
 SOCIALACCOUNT_ADAPTER = 'osf_oauth2_adapter.views.OSFOAuth2Adapter'
 SOCIALACCOUNT_PROVIDERS = \
     {'osf':
