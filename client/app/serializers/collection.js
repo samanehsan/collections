be02fc26094b28884cbb4b28b439d123d8183170
Saman Ehsan <se8ea@virginia.edu>
2017-05-08 17:26:28 -0400

    Handle settings as JSON field

--- client/app/serializers/collection.js
+++ client/app/serializers/collection.js
@@ -1,21 +1,4 @@
 import ApplicationSerializer from './application';
 
 export default ApplicationSerializer.extend({
-    normalizeResponse(store, primaryModelClass, payload) {
-        if(Array.isArray(payload.data)){
-            payload.data.forEach(function(collection){
-              collection.attributes.settings = JSON.parse(collection.attributes.settings);
-            });
-        } else {
-            payload.data.attributes.settings = JSON.parse(payload.data.attributes.settings);
-        }
-        return this._super(...arguments);
-    },
-    serialize(){
-        let json = this._super(...arguments);
-        if(typeof json.data.attributes.settings !== 'string'){
-            json.data.attributes.settings = JSON.stringify(json.data.attributes.settings);            
-        }
-        return json;
-    }
 });
