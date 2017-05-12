fbb07ffd310a7e893fb2c9ae1208e1ab10e3cc74
Saman Ehsan <se8ea@virginia.edu>
2017-05-12 16:26:35 -0400

    Add subject-picker to collections "add" page

--- client/app/controllers/collection/add.js
+++ client/app/controllers/collection/add.js
@@ -1,5 +1,25 @@
 import Ember from 'ember';
 
+// Helper function to determine if discipline has changed (comparing list of lists)
+function disciplineArraysEqual(a, b) {
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length !== b.length) return false;
+
+    for (let i = 0; i < a.length; ++i) {
+        if (a[i].length !== b[i].length) return false;
+        for (let j = 0; j < a[i].length; ++j) {
+            if (a[i][j] !== b[i][j]) return false;
+        }
+    }
+    return true;
+}
+
+function subjectIdMap(subjectArray) {
+    // Maps array of arrays of disciplines into array of arrays of discipline ids.
+    return subjectArray.map(subjectBlock => subjectBlock.map(subject => subject.id));
+}
+
 export default Ember.Controller.extend({
     addMethod: 'select', // 'select' or 'create'
     methodSelected: false,
@@ -7,6 +27,10 @@ export default Ember.Controller.extend({
         var collectionType = this.get('model.settings.collectionType') || 'project';
         return collectionType.toLowerCase();
     }),
+    editMode: false,
+    disciplineModifiedToggle: false,
+    disciplineChanged: true,
+    subjectsList: ['Math', 'Science'],
     actions:{
         updateProperty(oldValue, newValue){
             this.set(oldValue, newValue);
@@ -14,6 +38,18 @@ export default Ember.Controller.extend({
         },
         transition(name, id){
             this.transitionToRoute(name, id);
+        },
+        updateData(data, componentName) {
+          // Update section data
+          console.log(data);
+          this.set('subjectsList', data);
+          this.toggleProperty('disciplineModifiedToggle');
+          this.get('model').values[componentName] = data;
+        },
+        save() {
+            // TODO: Save the collection model
+            let model = this.get('model');
+            //model.save();
         }
     }
 });
--- client/app/routes/collection/add.js
+++ client/app/routes/collection/add.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
+var settings = {
+  sections: [
+    {
+      title: 'subject-picker',
+      settings: {
+        selected: ['math', 'science', 'art']
+      }
+    }
+  ],
+  values: {
+    'subject-picker': {},
+    'contributors': {}
+  }
+};
+
 export default Ember.Route.extend({
+    model() {
+      return settings;
+    }
 });
