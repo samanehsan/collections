0e11e0f017b126bddc30ea02d69895511c7b76f2
Saman Ehsan <se8ea@virginia.edu>
2017-07-13 14:07:45 -0400

    Return correct model in collection detail page

--- app/routes/collection.js
+++ app/routes/collection.js
@@ -1,11 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model (params) {
+    model(params) {
         return this.store.findRecord('collection', params.collection_id).then((data) => {
             return data;
         }).catch(() => {
-            this.transitionTo('/not-found');
+            this.store.findRecord('meeting', params.collection_id).then((data) => {
+                return data;
+            }).catch(() => {
+                this.transitionTo('/not-found');
+            });
         });
-    },
+    }
 });
