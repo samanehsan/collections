3dea3a0a5cb0991d8f091a8e4e31369994542087
Saman Ehsan <se8ea@virginia.edu>
2017-07-13 14:50:51 -0400

    Add form for creating a meeting
    TODO: Replace date inputs with datetimepicker

---
+++ app/controllers/create-meeting.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+    title: '',
+    selectedType: 'Preprint',
+    description: '',
+    location: '',
+    startDate: null,
+    endDate: null,
+    actions: {
+        addCollection () {
+            const selectedType = this.get('selectedType');
+            let meeting = this.store.createRecord('meeting', {
+                    title: this.get('title'),
+                    tags: '',
+                    settings: {collectionType: selectedType},
+                    description: this.get('description'),
+                    location: this.get('location'),
+                    startDate: this.get('startDate'),
+                    endDate: this.get('endDate')
+                });
+            meeting.save().then((record) => {
+                this.set('newCollectionTitle', '');
+                this.transitionToRoute('collection', record);
+            });
+        }
+    }
+});
--- app/router.js
+++ app/router.js
@@ -22,6 +22,7 @@ Router.map(function() {
   });
   this.route('create');
   this.route('not-found', { path: '/*path' });
+  this.route('create_meeting');
 });
 
 export default Router;
