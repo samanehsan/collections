cd6a11a77fe06b4e78f9f9ce1e256b7421e51f85
Saman Ehsan <se8ea@virginia.edu>
2017-07-13 14:21:14 -0400

    Add meetings home page
    Add route for meetings home pages that
    lists all meetings and includes a button
    for creating a new meeting.

--- app/router.js
+++ app/router.js
@@ -6,20 +6,22 @@ const Router = Ember.Router.extend({
     rootURL: config.rootURL,
 });
 
-Router.map(function() { // eslint-disable-line array-callback-return
-    this.route('index', { path: '/' });
-    this.route('collection', { path: 'collection/:collection_id' }, function() {
-        this.route('item', { path: 'item/:item_id' });
-        this.route('group', { path: 'group/:group_id' }, function() {
-            this.route('item', { path: 'item/:group_item_id' });
-        });
-        this.route('add');
-        this.route('search');
-        this.route('browse');
-        this.route('edit');
-    });
-    this.route('create');
-    this.route('not-found', { path: '/*path' });
+Router.map(function() {
+  // eslint-disable-line array-callback-return
+  this.route('index', { path: '/' });
+  this.route('meetings'); // home page
+  this.route('collection', { path: 'collection/:collection_id' }, function() {
+      this.route('item', { path: 'item/:item_id' });
+      this.route('group', { path: 'group/:group_id' }, function() {
+          this.route('item', { path: 'item/:group_item_id' });
+      });
+      this.route('add');
+      this.route('search');
+      this.route('browse');
+      this.route('edit');
+  });
+  this.route('create');
+  this.route('not-found', { path: '/*path' });
 });
 
 export default Router;
