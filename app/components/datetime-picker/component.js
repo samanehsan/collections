9d539eb3ae635bad3fe94f6595c07a0ac8412594
Saman Ehsan <se8ea@virginia.edu>
2017-07-19 13:21:10 -0400

    Simplify setting selected dates

--- app/components/datetime-picker/component.js
+++ app/components/datetime-picker/component.js
@@ -1,11 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    date: null,
     pickerValue: null,
-    changed: Ember.observer('date', function() {
-        this.set('pickerValue', this.get('date'));
-    }),
     didInsertElement() {
         this.$('.datetimepicker').datetimepicker();
     }
