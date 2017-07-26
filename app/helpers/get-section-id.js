6fff360e7a2b2349773afbfe120dfd94b912318b
Saman Ehsan <se8ea@virginia.edu>
2017-07-26 15:40:03 -0400

    Fix getting section ids
    Use the first word of the section title
    with all punctuation removed and spaces
    replaced with underscores as the section
    id.

--- app/helpers/get-section-id.js
+++ app/helpers/get-section-id.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export function getSectionId(params/*, hash*/) {
   const sectionHeader = params[0].split(' ');
-  return sectionHeader[0].replace(',', '');
+  return sectionHeader[0].replace(/[.,/#!$%^&*;:{}=`~()]/g,'').replace(/\s/g, '_');
 }
 
 export default Ember.Helper.helper(getSectionId);
