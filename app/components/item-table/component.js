54e155048311fefaf437c03e1066e6481a8f3c71
Saman Ehsan <se8ea@virginia.edu>
2017-07-31 15:51:59 -0400

    Make item table header css customizable

--- app/components/item-table/component.js
+++ app/components/item-table/component.js
@@ -1,5 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
+    theadStyle: Ember.computed('layout', function() {
+        const headerColor = this.get('layout.header_color') ? this.get('layout.header_color') : this.get('branding.colors.primary');
+        const textColor = this.get('layout.text_color') ? this.get('layout.text_color') : this.get('branding.colors.text');
+        return Ember.String.htmlSafe(`background-color: ${headerColor}; color: ${textColor};`);
+    })
 });
