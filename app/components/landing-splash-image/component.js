d91225022643177089ec0b9c14afc28ef5ac9745
Cameron <cam.blandford@gmail.com>
2017-07-31 11:11:24 -0400

    Remove unused data attribute

--- app/components/landing-splash-image/component.js
+++ app/components/landing-splash-image/component.js
@@ -2,10 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     session: Ember.inject.service(),
-    data: Ember.computed('layout', function() {
-        const dataSource = this.get('layout.data');
-        return this.get('model.settings').data[dataSource];
-    }),
     imageStyle: Ember.computed('layout', function() {
         let url = this.get('layout.img_url') ? "url(" + this.get('layout.img_url') + ")" : "url('/img/splash-default.jpg')";
         let height = this.get('layout.height') ? this.get('layout.height') + "px" : "300px";
