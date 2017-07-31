3327e621106bd6346a03d56be08caa2f5e180a2a
Cameron <cam.blandford@gmail.com>
2017-07-31 11:21:53 -0400

    Remove unused data attribute

--- app/components/landing-paragraph/component.js
+++ app/components/landing-paragraph/component.js
@@ -2,10 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     session: Ember.inject.service(),
-    data: Ember.computed('layout', function() {
-        const dataSource = this.get('layout.data');
-        return this.get('model.settings').data[dataSource];
-    }),
     containerStyle: Ember.computed('layout', function() {
         return Ember.String.htmlSafe(`background-color: ${this.get('layout.background_color')}; color: ${this.get('layout.text_color')}`);
     }),
