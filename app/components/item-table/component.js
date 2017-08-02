6af53beed054ae06de22ec00789230da482eee3e
Saman Ehsan <se8ea@virginia.edu>
2017-08-02 13:46:33 -0400

    Add filtering to presentation list
    Add filtering presentation list by title
    or by event type.

--- app/components/item-table/component.js
+++ app/components/item-table/component.js
@@ -1,9 +1,21 @@
 import Ember from 'ember';
+import _ from 'lodash';
 
 export default Ember.Component.extend({
     theadStyle: Ember.computed('layout', function() {
         const headerColor = this.get('layout.header_color') ? this.get('layout.header_color') : this.get('branding.colors.primary');
         const textColor = this.get('layout.text_color') ? this.get('layout.text_color') : this.get('branding.colors.text');
         return Ember.String.htmlSafe(`background-color: ${headerColor}; color: ${textColor};`);
+    }),
+    filterString: "",
+    eventFilter: "",
+    eventTypes: Ember.computed('model', function () {
+        return this.get('model.items').then((results) => {
+            let eventsList = [];
+            results.forEach(function (i) {
+                eventsList.push(i.get('type'));
+            });
+            return _.uniq(eventsList).sort();
+        });
     })
 });
